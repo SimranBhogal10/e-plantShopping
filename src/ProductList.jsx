6f4f47541c65ab9635f8f76a37082e2ad333f0a5
SimranBhogal10 <bhogalsimran42@gmail.com>
2025-03-15 14:17:20 -0400

    chore: added the count on the cart icon

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import CartItem from './CartItem';
 import {addItem} from './CartSlice';
 
@@ -9,6 +9,8 @@ function ProductList({ onHomeClick }) {
     const [addedToCart, setAddedToCart] = useState({});
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const dispatch = useDispatch();
+    const cartItems = useSelector(state => state.cart.items);
+    const totalItemsCount = cartItems.reduce((acc, item) => acc + item.quantity, 0);
 
     const plantsArray = [
         {
@@ -281,7 +283,24 @@ function ProductList({ onHomeClick }) {
                         </div>
                     </div>
                     <div style={{width: '300px'}}> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                    <div> 
+                        <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
+                            <h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1>
+                        </a>
+                        <span 
+                          style={{ 
+                            color: 'white', 
+                            padding: '2px 6px',
+                            position: 'absolute', 
+                            top: '36px', 
+                            right: '36px',
+                            zIndex: 10,
+                            fontWeight: 'bold'
+                          }}
+                        >
+                          {totalItemsCount}
+                        </span>
+                    </div>
                 </div>
             </div>
             {!showCart ? (
